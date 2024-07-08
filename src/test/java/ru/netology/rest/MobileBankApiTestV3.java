ddd675f9380214d700b6a6d5cd326332b9576022
Tatyana <tatyanalipch@yandex.ru>
2024-07-08 12:56:15 +0300

    build.gradle

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -24,6 +24,6 @@ class MobileBankApiTestV3 {
                 .body("", hasSize(3))
                 .body("[0].currency", equalTo("RUB"))
                 .body("[0].balance", greaterThanOrEqualTo(0))
-        ;
+                .body ("[0].id", equalTo(1) );
     }
 }
